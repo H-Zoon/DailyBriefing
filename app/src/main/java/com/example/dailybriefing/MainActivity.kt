9749a283095547bb8a895f85aa764042fa55b635
jun34723 <jun34723@avatye.com>
2022-10-12 16:11:53 +0900

    (feat)시계 구현

--- app/src/main/java/com/example/dailybriefing/MainActivity.kt
+++ app/src/main/java/com/example/dailybriefing/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.dailybriefing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.dailybriefing.databinding.ActivityMainBinding
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding //activity_main.xml
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        setContentView(binding.root)
+
+        viewModel.timeValue.observe(this) {
+            binding.time.text = it.toString()
+        }
+
+        viewModel.dateValue.observe(this) {
+            binding.day.text = it.toString()
+        }
+
     }
 }
