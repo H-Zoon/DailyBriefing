e8b7fb3fc2a6236efb502221b139333531c8e639
jun34723 <jun34723@avatye.com>
2022-10-12 16:50:56 +0900

    (feat)databinding 변경

--- app/src/main/java/com/example/dailybriefing/MainActivity.kt
+++ app/src/main/java/com/example/dailybriefing/MainActivity.kt
@@ -3,29 +3,16 @@ package com.example.dailybriefing
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import com.example.dailybriefing.databinding.ActivityMainBinding
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding //activity_main.xml
-    private lateinit var viewModel: MainViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        setContentView(binding.root)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        viewModel.timeValue.observe(this) {
-            binding.time.text = it.toString()
-        }
-
-        viewModel.dateValue.observe(this) {
-            binding.day.text = it.toString()
-        }
+        binding.timeData = MainViewModel()
 
     }
 }
--- app/src/main/java/com/example/dailybriefing/MainViewModel.kt
+++ app/src/main/java/com/example/dailybriefing/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.dailybriefing
 
-import androidx.lifecycle.LiveData
+import android.util.Log
+import androidx.databinding.ObservableField
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -10,23 +11,19 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class MainViewModel : ViewModel(){
-    private var _timeValue = MutableLiveData<String>()
-    private var _dateValue = MutableLiveData<String>()
-
-
-    val timeValue: LiveData<String>
-        get() = _timeValue
-
-    val dateValue: LiveData<String>
-        get() = _dateValue
+    var timeValue = ObservableField<String>()
+    var dateValue = ObservableField<String>()
 
     init {
         viewModelScope.launch {
             while (true) {
-                _timeValue.value = LocalDateTime.now().format(DateTimeFormatter.ofPattern("a h:m:ss"))
-                _dateValue.value = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy년 M월 d일"))
+                timeValue.set(LocalDateTime.now().format(DateTimeFormatter.ofPattern("a h:m:ss")))
+                dateValue.set(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy년 M월 d일")))
                 delay(1000)
+                Log.d("c", "${timeValue.get()}")
             }
         }
     }
+
+
 }
