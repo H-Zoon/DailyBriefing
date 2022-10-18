8f8e1eb1c20f6d810c89ca50174ffa6492571c19
jun34723 <jun34723@avatye.com>
2022-10-18 18:29:01 +0900

    (feat) 야간모드 기능 추가/ 브리핑 버튼 수정

--- app/src/main/java/com/example/dailybriefing/MainActivity.kt
+++ app/src/main/java/com/example/dailybriefing/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.dailybriefing
 
 import android.annotation.SuppressLint
+import android.content.SharedPreferences
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.transition.ChangeBounds
 import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.animation.AccelerateInterpolator
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.content.ContextCompat
 import com.example.dailybriefing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(App.context())
+    }
 
     lateinit var binding: ActivityMainBinding
 
@@ -27,7 +35,27 @@ class MainActivity : AppCompatActivity() {
         var briefingIsOpen = false
         val briefingFragment = BriefingFragment()
 
-        binding.button.setOnClickListener {
+        when (prefs.getString("theme", "auto")) {
+            "light" -> {
+                binding.setting.background =
+                    ContextCompat.getDrawable(this, R.drawable.outline_light_mode_24)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+            "dark" -> {
+                binding.setting.background =
+                    ContextCompat.getDrawable(this, R.drawable.outline_dark_mode_24)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+
+            "auto" -> {
+                binding.setting.background =
+                    ContextCompat.getDrawable(this, R.drawable.outline_hdr_auto_24)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+        }
+
+        binding.briefing.setOnClickListener {
             if (!briefingIsOpen) {
                 val transaction = supportFragmentManager.beginTransaction()
                     .replace(binding.briefingFrag.id, briefingFragment)
@@ -43,6 +71,10 @@ class MainActivity : AppCompatActivity() {
             briefingIsOpen = !briefingIsOpen
         }
 
+        binding.setting.setOnClickListener {
+            nightModeChanger()
+        }
+
     }
 
     private fun updateView(@LayoutRes id: Int) {
@@ -55,5 +87,37 @@ class MainActivity : AppCompatActivity() {
         TransitionManager.beginDelayedTransition(binding.root, trans)
     }
 
+    private fun nightModeChanger() {
+        when (prefs.getString("theme", "auto")) {
+            "light" -> {
+                binding.setting.background =
+                    ContextCompat.getDrawable(this, R.drawable.outline_dark_mode_24)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                with(prefs.edit()) {
+                    putString("theme", "dark")
+                }.apply()
+
+            }
+
+            "dark" -> {
+                binding.setting.background =
+                    ContextCompat.getDrawable(this, R.drawable.outline_hdr_auto_24)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                with(prefs.edit()) {
+                    putString("theme", "auto")
+                }.apply()
+            }
+
+            "auto" -> {
+                binding.setting.background =
+                    ContextCompat.getDrawable(this, R.drawable.outline_light_mode_24)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                with(prefs.edit()) {
+                    putString("theme", "light")
+                }.apply()
+
+            }
+        }
+    }
 }
 
